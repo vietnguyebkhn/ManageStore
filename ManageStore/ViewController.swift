23caafe510b3b6cd2e5ff9163d9d72567e3f4cdd
Việt Nguyễn <nguyenviet@vietnguyen.local>
2018-09-24 15:16:33 +0700

    Initial Commit

---
+++ ManageStore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ManageStore
+//
+//  Created by Nguyễn Việt on 9/24/18.
+//  Copyright © 2018 Việt Nguyễn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
